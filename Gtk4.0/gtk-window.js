fb2037aa6b4ccff3fdf4523bad53da92ae3ccefc
heinrichschiller <heinrich@heinrich-schiller.de>
2024-07-14 20:38:40 +0200

    added, gtk-window example

---
+++ Gtk4.0/gtk-window.js
@@ -0,0 +1,18 @@
+imports.gi.versions['Gtk'] = '4.0';
+
+const Gtk = imports.gi.Gtk;
+
+let app = new Gtk.Application({ application_id: 'org.gtk.exampleapp' });
+
+app.connect('activate', () => {
+    let window = new Gtk.ApplicationWindow({ 
+        application: app, 
+        defaultWidth: 600, 
+        defaultHeight: 400, 
+        title: 'Gtk4.0-Window example' 
+    });
+
+    window.present();
+});
+
+app.run([]);
