20c890bf4e3cfe321417c5ee1ab31048ad209fc8
heinrichschiller <heinrich@heinrich-schiller.de>
2024-07-21 19:41:24 +0200

    added, gtk-box example

---
+++ Gtk4.0/gtk-box.js
@@ -0,0 +1,33 @@
+imports.gi.versions['Gtk'] = '4.0';
+
+const Gtk = imports.gi.Gtk;
+
+const app = new Gtk.Application({ application_id: 'org.gtk.exampleapp'});
+
+app.connect('activate', () => {
+    const box = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL });
+
+    const label = new Gtk.Label({ label: 'Gtk4.0-Box' });
+
+    box.append(label);
+
+    const button = new Gtk.Button({ label: 'Click Me!' });
+    button.connect('clicked', () => {
+        log('This button was clicked.')
+    })
+
+    box.append(button);
+
+    const window = new Gtk.ApplicationWindow({
+        application: app,
+        defaultWidth: 600,
+        defaultHeight: 400,
+        title: 'Gtk4.0-Box'
+    });
+
+    window.connect('destroy', () => { Gtk.main_quit(); });
+    window.set_child(box);
+    window.present();
+});
+
+app.run([]);
